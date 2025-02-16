77e48556c78e4bf13b48937bf93e4b141ef492c2
Gupta <harshgupta38@deloitte.com>
2025-02-16 23:43:45 +0530

    fixed notification hiding unhiding feature

--- src/app/common/notification/notification.component.ts
+++ src/app/common/notification/notification.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, OnInit, Renderer2 } from '@angular/core';
 import { NotificationService } from '../../services/notification.service';
 import { CommonModule } from '@angular/common';
 
@@ -10,8 +10,24 @@ import { CommonModule } from '@angular/common';
   templateUrl: './notification.component.html',
   styleUrl: './notification.component.scss'
 })
-export class NotificationComponent {
+export class NotificationComponent implements AfterViewInit {
+
   constructor(
-    public notificationService: NotificationService
+    public notificationService: NotificationService,
+    private renderer: Renderer2
   ) { }
+
+  currentPosition: string = 'right';
+
+  ngAfterViewInit(): void {
+    const notificationContainer = document.querySelector('.notification-container');
+
+    if (notificationContainer) {
+      this.renderer.listen(notificationContainer, 'mouseover', () => {
+        this.renderer.removeClass(notificationContainer, this.currentPosition === 'right' ? 'moved-right' : 'moved-left');
+        this.renderer.addClass(notificationContainer, this.currentPosition === 'right' ? 'moved-left' : 'moved-right');
+        this.currentPosition = this.currentPosition === 'right' ? 'left' : 'right';
+      });
+    }
+  }
 }
