63e81722ae417913691e42ebb9efeea4eea7a693
Gupta <harshgupta38@deloitte.com>
2025-02-18 19:07:04 +0530

    fixed test cases in authentication component

--- src/app/authentication/authentication.component.spec.ts
+++ src/app/authentication/authentication.component.spec.ts
@@ -1,36 +1,56 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { RouterTestingModule } from '@angular/router/testing';
-import { ActivatedRoute } from '@angular/router';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { AuthenticationComponent } from './authentication.component';
-import { environment } from '../../environments/environment';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
+import { ReactiveFormsModule, Validators } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { NgxMaskDirective, provideNgxMask } from 'ngx-mask';
 import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { Database, getDatabase, provideDatabase } from '@angular/fire/database';
-import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { ActivatedRoute, Router } from '@angular/router';
+import { AccountService } from '../services/account.service';
+import { FirebaseService } from '../services/firebase.service';
+import { NotificationService } from '../services/notification.service';
+import { Database } from '@angular/fire/database';
+import { FIREBASE_OPTIONS } from '@angular/fire/compat';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { environment } from '../../environments/environment';
 
-fdescribe('AuthenticationComponent', () => {
+describe('AuthenticationComponent', () => {
   let component: AuthenticationComponent;
   let fixture: ComponentFixture<AuthenticationComponent>;
+  let mockRouter = { navigate: jasmine.createSpy('navigate') };
+  let mockAccountService = { hasUserData: jasmine.createSpy('hasUserData').and.returnValue(false) };
+  let mockFirebaseService = { getData: jasmine.createSpy('getData').and.returnValue(Promise.resolve({ key: 'testKey', number: '1234567890' })) };
+  let mockNotificationService = { showNotification: jasmine.createSpy('showNotification') };
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
+  beforeEach(waitForAsync(() => {
+    TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFireDatabaseModule,
-        AuthenticationComponent,
+        CommonModule,
+        ReactiveFormsModule,
+        MatButtonModule,
+        MatFormFieldModule,
+        MatInputModule,
+        NgxMaskDirective,
+        AuthenticationComponent
       ],
       providers: [
+        provideNgxMask(),
+        provideAnimations(),
+        { provide: AngularFireAuth, useValue: jasmine.createSpyObj('AngularFireAuth', ['signInWithPhoneNumber', 'signOut']) },
+        { provide: Router, useValue: mockRouter },
+        { provide: AccountService, useValue: mockAccountService },
+        { provide: FirebaseService, useValue: mockFirebaseService },
+        { provide: NotificationService, useValue: mockNotificationService },
+        { provide: Database, useValue: {} },
         { provide: ActivatedRoute, useValue: {} },
-        AngularFireAuth,
-        provideFirebaseApp(() => initializeApp(environment.firebaseConfig)),
-        provideDatabase(() => getDatabase())
-        // { provide: Database, useFactory: () => getDatabase() }
+        { provide: FIREBASE_OPTIONS, useValue: environment.firebaseConfig}
       ]
-    })
-      .compileComponents();
+    }).compileComponents();
+  }));
 
+  beforeEach(() => {
     fixture = TestBed.createComponent(AuthenticationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -38,5 +58,71 @@ fdescribe('AuthenticationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should initialize login form', () => {
+    expect(component.loginForm).toBeDefined();
+    expect(component.loginForm.get('phoneNumber')).toBeDefined();
+    expect(component.loginForm.get('otp')).toBeDefined();
+  });
+
+  it('should initialize signup form', () => {
+    expect(component.signupForm).toBeDefined();
+    expect(component.signupForm.get('fullName')).toBeDefined();
+    expect(component.signupForm.get('phoneNumber')).toBeDefined();
+    expect(component.signupForm.get('accessKey')).toBeDefined();
+    expect(component.signupForm.get('otp')).toBeDefined();
+  });
+
+  it('should navigate to dashboard if user data exists', waitForAsync(() => {
+    mockAccountService.hasUserData.and.returnValue(true);
+    component.ngOnInit();
+    fixture.whenStable().then(() => {
+      expect(mockRouter.navigate).toHaveBeenCalledWith(['/dashboard']);
+    });
+  }));
+
+  it('should set accessKey on init', waitForAsync(() => {
+    component.ngOnInit();
+    fixture.whenStable().then(() => {
+      expect(component.accessKey).toEqual({ key: 'testKey', number: '1234567890' });
+    });
+  }));
+
+  it('should switch to signup form on createNewAccBtnClick', () => {
+    component.isLogin = true;
+    component.createNewAccBtnClick();
+    expect(component.isLogin).toBeFalse();
+    expect(component.activeFormGroup).toBe(component.signupForm);
+  });
+
+  it('should switch to login form on loginBtnClick', () => {
+    component.isLogin = false;
+    component.loginBtnClick();
+    expect(component.isLogin).toBeTrue();
+    expect(component.activeFormGroup).toBe(component.loginForm);
+  });
+
+  it('should show error message for invalid phone number', () => {
+    component.activeFormGroup.get('phoneNumber')?.setErrors({ 'required': true });
+    expect(component.getErrorMessage('phoneNumber')).toBe('Please enter your contact number.');
+  });
+
+  it('should show error message for invalid access key', () => {
+    component.createNewAccBtnClick();
+    component.activeFormGroup.get('accessKey')?.setErrors({ 'required': true });
+    expect(component.getErrorMessage('accessKey')).toBe('Please enter your access key.');
+  });
+
+  it('should show error message for invalid OTP', () => {
+    component.activeFormGroup.get('otp')?.setErrors({ 'required': true });
+    expect(component.getErrorMessage('otp')).toBe('Please enter OTP.');
+  });
+
+  it('should update OTP sent status', () => {
+    component.updateOtpSentStatus(true);
+    expect(component.isOtpSent).toBeTrue();
+    expect(component.activeFormGroup.get('phoneNumber')?.disabled).toBeTrue();
+    expect(component.activeFormGroup.get('otp')?.hasValidator(Validators.required)).toBeTrue();
   });
 });
