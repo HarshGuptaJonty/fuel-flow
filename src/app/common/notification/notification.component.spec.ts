3aae01916eb04d3cb2d7a4370d37f2c50c10176b
Gupta <harshgupta38@deloitte.com>
2025-02-18 20:08:01 +0530

    fixed test cases in notification component

--- src/app/common/notification/notification.component.spec.ts
+++ src/app/common/notification/notification.component.spec.ts
@@ -1,17 +1,32 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 import { NotificationComponent } from './notification.component';
+import { NotificationService } from '../../services/notification.service';
+import { Renderer2 } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
-describe('NotificationComponent', () => {
+fdescribe('NotificationComponent', () => {
   let component: NotificationComponent;
   let fixture: ComponentFixture<NotificationComponent>;
+  let mockNotificationService: jasmine.SpyObj<NotificationService>;
+  let mockRenderer: jasmine.SpyObj<Renderer2>;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [NotificationComponent]
-    })
-    .compileComponents();
+  beforeEach(waitForAsync(() => {
+    mockNotificationService = jasmine.createSpyObj('NotificationService', ['']);
+    mockRenderer = jasmine.createSpyObj('Renderer2', ['listen', 'removeClass', 'addClass']);
 
+    TestBed.configureTestingModule({
+      imports: [
+        CommonModule,
+        NotificationComponent
+      ],
+      providers: [
+        { provide: NotificationService, useValue: mockNotificationService },
+        { provide: Renderer2, useValue: mockRenderer }
+      ]
+    }).compileComponents();
+  }));
+
+  beforeEach(() => {
     fixture = TestBed.createComponent(NotificationComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -19,5 +34,29 @@ describe('NotificationComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should toggle class on mouseover', () => {
+    const notificationContainer = document.createElement('div');
+    notificationContainer.classList.add('notification-container');
+    document.body.appendChild(notificationContainer);
+
+    mockRenderer.listen.and.callFake((target, event, callback) => {
+      target.addEventListener(event, callback);
+      return () => target.removeEventListener(event, callback);
+    });
+
+    spyOn(document, 'querySelector').and.returnValue(notificationContainer);
+
+    component.ngAfterViewInit();
+
+    const mouseoverEvent = new Event('mouseover');
+    notificationContainer.dispatchEvent(mouseoverEvent);
+    expect(component.currentPosition).toBe('left');
+
+    notificationContainer.dispatchEvent(mouseoverEvent);
+    expect(component.currentPosition).toBe('right');
+
+    document.body.removeChild(notificationContainer);
   });
 });
