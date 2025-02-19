ae15792c59a59130481d3c89f1eb9cd10cfb6aa1
Gupta <harshgupta38@deloitte.com>
2025-02-19 11:01:25 +0530

    fixed test case in admin data service

--- src/app/services/admin-data.service.spec.ts
+++ src/app/services/admin-data.service.spec.ts
@@ -1,30 +1,82 @@
 import { TestBed } from '@angular/core/testing';
-import { AngularFireModule } from '@angular/fire/compat';
-import { AngularFireAuth } from '@angular/fire/compat/auth';
-import { AngularFireDatabaseModule } from '@angular/fire/compat/database';
-import { environment } from '../../environments/environment';
 import { AdminDataService } from './admin-data.service';
-import { Database } from '@angular/fire/database';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
+import { FirebaseService } from './firebase.service';
 
 describe('AdminDataService', () => {
   let service: AdminDataService;
+  let mockAngularFireAuth: jasmine.SpyObj<AngularFireAuth>;
+  let mockFirebaseService: jasmine.SpyObj<FirebaseService>;
 
   beforeEach(() => {
+    mockAngularFireAuth = jasmine.createSpyObj('AngularFireAuth', ['signOut']);
+    mockFirebaseService = jasmine.createSpyObj('FirebaseService', ['getData']);
+
     TestBed.configureTestingModule({
-      imports: [
-        AngularFireModule.initializeApp(environment.firebaseConfig),
-        AngularFireDatabaseModule
-      ],
       providers: [
-        AngularFireAuth,
         AdminDataService,
-        { provide: Database, useValue: {} }
+        { provide: AngularFireAuth, useValue: mockAngularFireAuth },
+        { provide: FirebaseService, useValue: mockFirebaseService }
       ]
     });
+
     service = TestBed.inject(AdminDataService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initialize with data', async () => {
+    const adminData = { 'user1': { data: { fullName: 'Admin User' } } };
+    mockFirebaseService.getData.and.returnValue(Promise.resolve(adminData));
+
+    await service['initialize']();
+
+    service.adminList$.subscribe(data => {
+      expect(data).toEqual(adminData);
+    });
+
+    service.isDataChanged.subscribe(changed => {
+      expect(changed).toBeTrue();
+    });
+  });
+
+  it('should initialize with no data', async () => {
+    mockFirebaseService.getData.and.returnValue(Promise.resolve({}));
+
+    await service['initialize']();
+
+    service.adminList$.subscribe(data => {
+      expect(data).toBeNull();
+    });
+
+    service.isDataChanged.subscribe(changed => {
+      expect(changed).toBeFalse();
+    });
+  });
+
+  it('should get admin data by user ID', () => {
+    const adminData = { 'user1': { data: { fullName: 'Admin User' } } };
+    service['adminList'].next(adminData);
+
+    expect(service.getAdminData('user1')).toEqual(adminData['user1']);
+    expect(service.getAdminData('user2')).toBeUndefined();
+  });
+
+  it('should return null if user ID is not provided', () => {
+    expect(service.getAdminData()).toBeNull();
+  });
+
+  it('should get admin name by user ID', () => {
+    const adminData = { 'user1': { data: { fullName: 'Admin User' } } };
+    service['adminList'].next(adminData);
+
+    expect(service.getAdminName('user1')).toBe('Admin User');
+    expect(service.getAdminName('user2')).toBeUndefined();
+  });
+
+  it('should return "NA" if user ID is not provided', () => {
+    expect(service.getAdminName()).toBe('NA');
   });
 });
